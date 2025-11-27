100d274285926e1b5a71027523050574823f1bd9
cmd-daniel <cmd.princedaniel@gmail.com>
2025-11-27 06:46:35 +0000

    refactor: Simplify Dice component structure by replacing inline styles with CSS classes, enhancing maintainability and visual consistency. Updated Dice.module.css for improved styling and dimensions.

--- client/src/components/BetterDice.tsx
+++ client/src/components/BetterDice.tsx
@@ -3,109 +3,24 @@
 
 import React from "react";
 import { Quaternion } from "@/lib/diceMath";
-import styles from '@components/Dice.module.css'
+import styles from "@components/Dice.module.css";
 
 export interface DiceProps {
   quaternion: Quaternion;
 }
 
-const faceStyle: React.CSSProperties = {
-  position: "absolute",
-  width: "100px",
-  height: "100px",
-  border: "4px solid white",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  fontSize: "80px",
-  fontWeight: "bold",
-  color: "white"
-};
-
 export const Dice: React.FC<DiceProps> = ({ quaternion }) => {
   const transform = quaternion.toCSSMatrix();
 
   return (
-    <div
-      style={{
-        transformStyle: "preserve-3d",
-        transform: "rotateX(-60deg) rotateY(45deg)"
-      }}
-    >
-      <div
-        style={{
-          width: "100px",
-          height: "100px",
-          position: "relative",
-          transformStyle: "preserve-3d",
-          transform
-        }}
-      >
-        {/* Front - Face 4 */}
-        <div
-          style={{
-            ...faceStyle,
-            background: "#aa96da",
-            transform: "rotateY(0deg) translateZ(50px)"
-          }}
-        >
-          4
-        </div>
-
-        {/* Back - Face 3 */}
-        <div
-          style={{
-            ...faceStyle,
-            background: "#f38181",
-            transform: "rotateY(180deg) translateZ(50px)"
-          }}
-        >
-          3
-        </div>
-
-        {/* Right - Face 5 */}
-        <div
-          style={{
-            ...faceStyle,
-            background: "#4ecdc4",
-            transform: "rotateY(90deg) translateZ(50px)"
-          }}
-        >
-          5
-        </div>
-
-        {/* Left - Face 2 */}
-        <div
-          style={{
-            ...faceStyle,
-            background: "#ff6b6b",
-            transform: "rotateY(-90deg) translateZ(50px)"
-          }}
-        >
-          2
-        </div>
-
-        {/* Top - Face 6 */}
-        <div
-          style={{
-            ...faceStyle,
-            background: "#ffe66d",
-            transform: "rotateX(90deg) translateZ(50px)"
-          }}
-        >
-          6
-        </div>
-
-        {/* Bottom - Face 1 */}
-        <div
-          style={{
-            ...faceStyle,
-            background: "#95e1d3",
-            transform: "rotateX(-90deg) translateZ(50px)"
-          }}
-        >
-          1
-        </div>
+    <div className={styles.diceWrap}>
+      <div className={styles.dice} style={{ transform }}>
+        <div className={`${styles.diceFace} ${styles.face4}`}></div>
+        <div className={`${styles.diceFace} ${styles.face3}`}></div>
+        <div className={`${styles.diceFace} ${styles.face5}`}></div>
+        <div className={`${styles.diceFace} ${styles.face2}`}></div>
+        <div className={`${styles.diceFace} ${styles.face6}`}></div>
+        <div className={`${styles.diceFace} ${styles.face1}`}></div>
       </div>
     </div>
   );
