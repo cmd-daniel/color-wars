7c4901543ebcf71110c8c754ec1391e211d45673
cmd-daniel <cmd.daniel.work@gmail.con>
2025-12-21 11:05:00 +0000

    fix(ActionArea): adjust minimum height class for better layout consistency

--- client/src/components/ActionArea.tsx
+++ client/src/components/ActionArea.tsx
@@ -9,7 +9,7 @@ const ActionArea = ({ children }: ActionAreaProps) => {
   return (
     <div className="fixed right-0 bottom-0 left-0 flex w-full justify-center ">
       <div className="flex w-full flex-col gap-0.5 pb-2 px-2 max-w-[720px] bg-background">
-        <div className="bg-secondary flex min-h-[16vh] items-center justify-center rounded-xl drop-shadow-[0px_-8px_8px_#000000] p-4 z-10">
+        <div className="bg-secondary flex min-h-30 h-[16vh] max-h-45 items-center justify-center rounded-xl drop-shadow-[0px_-8px_8px_#000000] p-4 z-10">
           {children}
         </div>
         <div className="bg-secondary flex h-12 items-center justify-center rounded-lg">
